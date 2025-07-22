c7044e7fa9e1358ce5b40a1b58637cea1f2072c7
korsh <korshak1962@gmail.com>
2025-07-22 16:27:41 -0700

    charstream

--- src/com/slava/infosys/CharStream.java
+++ src/com/slava/infosys/CharStream.java
@@ -1,31 +1,48 @@
 package com.slava.infosys;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CharStream {
 
-    public String FirstNonRepeating(String s) {
-        int i =0;
-        Set<Character> repeating = new HashSet<>();
-        Map<Character,Integer> nonRepeating = new HashMap<>();
-        StringBuilder builder  = new StringBuilder();
-
-        for (int i=0;i<s.length();i++){
-            if (repeating.contains(s.charAt(i))) {continue;
-
-            }
-            if (nonRepeating.add(s.charAt(i))==null)){
-                repeating.add(s.charAt(i));
-                nonRepeating.remove(s.charAt(i));
-                if (nonRepeating.isEmpty()){
-                    hashInd.add(i);
-                }
-            }
+  public String FirstNonRepeating(String s) {
+    Set<Character> repeating = new HashSet<>();
+    LinkedHashSet<Character> nonRepeating =
+        new LinkedHashSet<>();
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < s.length(); i++) {
+      if (repeating.contains(s.charAt(i))) {
+        builder.append(builder.charAt(builder.length() - 1));
+        continue;
+      }
+      if (nonRepeating.contains(s.charAt(i))) {
+        repeating.add(s.charAt(i));
+        nonRepeating.remove(s.charAt(i));
+        if (nonRepeating.isEmpty()) {
+          builder.append("#");
         }
+      } else {
+        nonRepeating.add(s.charAt(i));
+        builder.append(nonRepeating.getFirst());
+      }
     }
+    return builder.toString();
+  }
 
+  @Test
+  public void test() {
+    String s = "aabc";
+    String out = FirstNonRepeating(s);
+    Assert.assertEquals("a#bb", out);
+  }
 
+  @Test
+  public void test1() {
+    String s = "yewaahkpuo";
+    String out = FirstNonRepeating(s);
+    Assert.assertEquals("yyyyyyyyyy", out);;
+  }
 }
